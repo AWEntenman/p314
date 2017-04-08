3971b0a96612117f46111af8bd132d07f67ed859
Alan <awe@cosecom.com>
2017-04-08 16:03:17 -0400

    best 36 point I can find

--- p314/stdafx.h
+++ p314/stdafx.h
@@ -9,10 +9,9 @@
 #include <stdio.h>
 #include <tchar.h>
 #include <iostream>
-#include <fstream>
+#include <iomanip>
 
 #include <time.h>
-
 
 
 // TODO: reference additional headers your program requires here
