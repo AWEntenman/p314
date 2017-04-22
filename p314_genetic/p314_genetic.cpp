d69dc9c57a77747f74be8ee90f3a4bb3196fb033
Alan <awe@cosecom.com>
2017-04-22 11:52:31 -0400

    Tested algorithm with crossover and mutation in place, good solution found

--- p314_genetic/p314_genetic.cpp
+++ p314_genetic/p314_genetic.cpp
@@ -12,28 +12,32 @@ using namespace std;
 #define D         60   //Number of parents
 #define Le        15   //Number of generations with no improvement for extinction
 #define Lt        100  //Number of generations for convergence
-#define Pm        0.01 //Probability of a mutation
+#define Pm        4    //Probability / 256 of a mutation
 #define rho       30   //Number of parents kept for the next generation
 #define ggen_size N    //
 #define ggen_indx 19   //
-double maxSo = 132.520;
+double maxSo = 132.20;
 
 struct gen314 {
 	double opt;
 	unsigned short xy[ggen_indx];
-} ggen[ggen_size]{}, tgen[ggen_size]{};
+} ggen[ggen_size]{}, tgen[D]{};
 
 int compareD(const void *i, const void *j) {
 	double a, b;
 	a = *(double *)i;
 	b = *(double *)j;
-	if (a>=b)
+	if (a < b)
 	{
-		return -1;
+		return 1;
+	}
+	else if (a == b)
+	{
+		return 0;
 	}
 	else
 	{
-		return 1;
+		return -1;
 	}
 }
 
@@ -41,19 +45,32 @@ int compareI(const void *i, const void *j) {
 	unsigned short a, b;
 	a = *(unsigned short *)i;
 	b = *(unsigned short *)j;
-	return (int)(a - b);
+	if (a > b)
+	{
+		return 1;
+	}
+	else if (a == b)
+	{
+		return 0;
+	}
+	else
+	{
+		return -1;
+	}
 }
 
 int main()
 {
-	//srand(12345);
-	for (; ;)
+	//compute the area/perimeter ratio for all edges
+	for (int i = 0; i < ggen_size; i++)
 	{
-		//compute the area/perimeter ratio for all edges
-		for (int i = 0; i < ggen_size; i++)
+		int t1[ggen_indx];
+		int t2[2 * ggen_indx];
+		double a = 0;
+		double ll = 0;
+		srand(12345);
+		//populate ggen
 		{
-			//populate ggen
-			int t1[ggen_indx];
 			for (int j = 0; j < ggen_size; j++)
 			{
 				t1[0] = pr8() / 4;
@@ -74,14 +91,184 @@ int main()
 					t1[k] = t1[k] / t1[ggen_indx - 1];
 				}
 			}
-			//qsort((void *)t1, ggen_indx-1, sizeof(int), compareI);
 			copy(begin(t1), end(t1), begin(ggen[i].xy));
+		}
+		//Build the coefficient list
+		{
+			for (int j = 1; j < ggen_indx; j++)
+			{
+				t2[j] = xy[t1[j - 1]][0];
+				t2[2 * ggen_indx - 1 - j] = xy[t1[j - 1]][1];
+			}
+			t2[0] = 0;
+			t2[2 * ggen_indx - 2] = 250;
+			t2[2 * ggen_indx - 1] = 250;
+		}//compute the objective function
+		 //  compute the area
+		{
+			for (int j = 0; j < 2 * ggen_indx - 2; j++)
+			{
+				a += (t2[j + 1] - t2[j])*(t2[2 * ggen_indx - 1 - j] + t2[2 * ggen_indx - 2 - j]);
+			}
+			a /= 2;
+		}
+		//  compute the length
+		{
+			for (int j = 0; j < ggen_indx - 1; j++)
+			{
+				ll += sqrt((t2[j + 1] - t2[j])*(t2[j + 1] - t2[j])
+					+ (t2[2 * ggen_indx - 1 - j] - t2[2 * ggen_indx - 2 - j])*(t2[2 * ggen_indx - 1 - j] - t2[2 * ggen_indx - 2 - j]));
+			}
+			ll += ll;
+			ll += sqrt(2 * (t2[ggen_indx] - t2[ggen_indx - 1])*(t2[ggen_indx] - t2[ggen_indx - 1]));
+		}
+		//store the objective function
+		ggen[i].opt = a / ll;
+	}
+	//sort ggen
+	qsort((void *)ggen, ggen_size, sizeof(gen314), compareD);
+
+
+	for (; ;)
+	{
+		//Build the next generation from D parents,
+		// copy best D parents to tgenn for mating
+		for (int i = 0; i < D; i++)
+		{
+			tgen[i] = ggen[i];
+		}
+		// Mate D parents to create (N - D + rho) children
+		// keep rho parents.
+		for (int i = rho; i < ggen_size; i += 2)
+		{
+			//Place 2 children into ggen from 2 parents in tgen
+			//Use crossover and mutation
+			int r, s, t;
+			//make 2 vars 0 <= r,s < D to pick 2 parents
+			r = D*pr8() >> 8;
+			s = D*pr8() >> 8;
+			//make crossover point
+			t = (ggen_indx - 1)*pr8() >> 8;
+			for (int j = 0; j < t; j++)
+			{
+				ggen[i].xy[j] = tgen[r].xy[j];
+				if (pr8() < Pm)
+				{
+					short g = pr8();
+					if (g < 64)
+					{
+						g = tgen[r].xy[j] + 1;
+					}
+					else if (g < 128)
+					{
+						g = tgen[r].xy[j] + 2;
+					}
+					else if (g < 192)
+					{
+						g = tgen[r].xy[j] - 1;
+					}
+					else
+					{
+						g = tgen[r].xy[j] - 2;
+					}
+					if ((g < ggen_indx) && (g >= 0))
+					{
+						ggen[i].xy[j] = g;
+					}
+				}
+				ggen[i + 1].xy[j] = tgen[s].xy[j];
+				if (pr8() < Pm)
+				{
+					short g = pr8();
+					if (g < 64)
+					{
+						g = tgen[s].xy[j] + 1;
+					}
+					else if (g < 128)
+					{
+						g = tgen[s].xy[j] + 2;
+					}
+					else if (g < 192)
+					{
+						g = tgen[s].xy[j] - 1;
+					}
+					else
+					{
+						g = tgen[s].xy[j] - 2;
+					}
+					if ((g < ggen_indx) && (g >= 0))
+					{
+						ggen[i + 1].xy[j] = g;
+					}
+				}
+			}
+
+			for (int j = t; j < ggen_indx; j++)
+			{
+				ggen[i].xy[j] = tgen[s].xy[j];
+				if (pr8() < Pm)
+				{
+					short g = pr8();
+					if (g < 64)
+					{
+						g = tgen[s].xy[j] + 1;
+					}
+					else if (g < 128)
+					{
+						g = tgen[s].xy[j] + 2;
+					}
+					else if (g < 192)
+					{
+						g = tgen[s].xy[j] - 1;
+					}
+					else
+					{
+						g = tgen[s].xy[j] - 2;
+					}
+					if ((g < ggen_indx) && (g >= 0))
+					{
+						ggen[i].xy[j] = g;
+					}
+				}
+				ggen[i + 1].xy[j] = tgen[r].xy[j];
+				if (pr8() < Pm)
+				{
+					short g = pr8();
+					if (g < 64)
+					{
+						g = tgen[r].xy[j] + 1;
+					}
+					else if (g < 128)
+					{
+						g = tgen[r].xy[j] + 2;
+					}
+					else if (g < 192)
+					{
+						g = tgen[r].xy[j] - 1;
+					}
+					else
+					{
+						g = tgen[r].xy[j] - 2;
+					}
+					if ((g < ggen_indx) && (g >= 0))
+					{
+						ggen[i].xy[j] = g;
+					}
+				}
+			}
+			qsort((void *)ggen[i].xy, ggen_indx - 1, sizeof(unsigned short), compareI);
+			qsort((void *)ggen[i + 1].xy, ggen_indx - 1, sizeof(unsigned short), compareI);
+			// *******************************************************************************
+			//
+			//compute area/perimeter ratio for ggen[i].opt
+			//
+			// *******************************************************************************
 			//Build the coefficient list
 			int t2[2 * ggen_indx];
 			for (int j = 1; j < ggen_indx; j++)
 			{
-				t2[j] = xy[t1[j-1]][0];
-				t2[2 * ggen_indx - 1 - j] = xy[t1[j-1]][1];
+				t2[j] = xy[ggen[i].xy[j - 1]][0];
+				t2[2 * ggen_indx - 1 - j] = xy[ggen[i].xy[j - 1]][1];
 			}
 			t2[0] = 0;
 			t2[2 * ggen_indx - 2] = 250;
@@ -89,55 +276,61 @@ int main()
 			//compute the objective function
 			//  compute the area
 			double a = 0;
-			for (int j = 0; j < 2*ggen_indx-2; j++)
+			for (int j = 0; j < 2 * ggen_indx - 2; j++)
 			{
-				a += (t2[j + 1] - t2[j])*(t2[2*ggen_indx-1-j] + t2[2*ggen_indx-2-j]);
+				a += (t2[j + 1] - t2[j])*(t2[2 * ggen_indx - 1 - j] + t2[2 * ggen_indx - 2 - j]);
 			}
 			a /= 2;
 			//  compute the length
 			double ll = 0;
-			for (int j = 0; j < ggen_indx-1; j++)
+			for (int j = 0; j < ggen_indx - 1; j++)
 			{
 				ll += sqrt((t2[j + 1] - t2[j])*(t2[j + 1] - t2[j])
-					+ (t2[2*ggen_indx - 1 - j] - t2[2*ggen_indx - 2 - j])*(t2[2*ggen_indx - 1 - j] - t2[2*ggen_indx - 2 - j]));
+					+ (t2[2 * ggen_indx - 1 - j] - t2[2 * ggen_indx - 2 - j])*(t2[2 * ggen_indx - 1 - j] - t2[2 * ggen_indx - 2 - j]));
 			}
 			ll += ll;
 			ll += sqrt(2 * (t2[ggen_indx] - t2[ggen_indx - 1])*(t2[ggen_indx] - t2[ggen_indx - 1]));
 			//store the objective function
-			ggen[i].opt=a/ll;
+			ggen[i].opt = a / ll;
+			// *******************************************************************************
+			//
+			//compute area/perimeter ratio for ggen[i+1].opt
+			//
+			// *******************************************************************************
+			//Build the coefficient list
+			for (int j = 1; j < ggen_indx; j++)
+			{
+				t2[j] = xy[ggen[i + 1].xy[j - 1]][0];
+				t2[2 * ggen_indx - 1 - j] = xy[ggen[i + 1].xy[j - 1]][1];
+			}
+			t2[0] = 0;
+			t2[2 * ggen_indx - 2] = 250;
+			t2[2 * ggen_indx - 1] = 250;
+			//compute the objective function
+			//  compute the area
+			a = 0;
+			for (int j = 0; j < 2 * ggen_indx - 2; j++)
+			{
+				a += (t2[j + 1] - t2[j])*(t2[2 * ggen_indx - 1 - j] + t2[2 * ggen_indx - 2 - j]);
+			}
+			a /= 2;
+			//  compute the length
+			ll = 0;
+			for (int j = 0; j < ggen_indx - 1; j++)
+			{
+				ll += sqrt((t2[j + 1] - t2[j])*(t2[j + 1] - t2[j])
+					+ (t2[2 * ggen_indx - 1 - j] - t2[2 * ggen_indx - 2 - j])*(t2[2 * ggen_indx - 1 - j] - t2[2 * ggen_indx - 2 - j]));
+			}
+			ll += ll;
+			ll += sqrt(2 * (t2[ggen_indx] - t2[ggen_indx - 1])*(t2[ggen_indx] - t2[ggen_indx - 1]));
+			//store the objective function
+			ggen[i + 1].opt = a / ll;
 		}
-		//sort ggen
+		// quick sort ggen
 		qsort((void *)ggen, ggen_size, sizeof(gen314), compareD);
 
-		//Build the next generation from D parents,
-		// keep rho parents.
-		for (int i = 0; i < rho; i++)
-		{
-			tgen[i] = ggen[i];
-		}
-		// Mate D parents to create D - rho children
-		for (int i = rho; i < ggen_size; i+2)
-		{
-			tgen[i] = ggen[r];
-
-
-			qsort((void *)tgen[i].xy, ggen_indx - 1, sizeof(int), compareI);
-
-
-
-
-			tgen[i + 1] = ggen[s];
-			qsort((void *)tgen[i+1].xy, ggen_indx - 1, sizeof(int), compareI);
-		}
-		
-		
-
-
-
-
-
-
-		copy(begin(tgen), end(tgen), begin(ggen));
+		//tgen[i + 1] = ggen[s];
+		//copy(begin(tgen), end(tgen), begin(ggen));
 
 		//Display ggen
 		if (ggen[0].opt > maxSo)
@@ -151,5 +344,4 @@ int main()
 		}
 	}
 	return 0;
-
 }
