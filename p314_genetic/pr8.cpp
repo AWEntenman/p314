acaade19bd237ebef647db1ccbd5d6691ad2e94e
Alan <awe@cosecom.com>
2017-04-25 05:59:02 -0400

    added __int8 instead of  short.

--- p314_genetic/pr8.cpp
+++ p314_genetic/pr8.cpp
@@ -17,27 +17,27 @@ unsigned short pr8()
 #define b  161 //must be an odd number
 #define m  181 // m mod 4 = 1
 
-	static unsigned short S[256];
-	static unsigned short r;
-	static unsigned short s;
+	static unsigned __int8 S[256];
+	static unsigned __int8 r;
+	static unsigned __int8 s;
 	static bool started = false;
 
-	unsigned short t1;
+	unsigned __int8 t1;
 
 	if (!started) {
 		//initialize S using a linear congruential pseudorandom number generator
 		S[0] = m0;
 		for (int i = 1; i < 256; i++)
 		{
-			S[i] = (b + m*S[i - 1]) & 0xff;
+			S[i] = b + m*S[i - 1];
 		}
 		r = r0;
 		s = s0;
 		//remove any initialization regularities, if any.
 		for (int i = 0; i < c0; i++)
 		{
-			r = (r + 1) & 0xff;
-			s = (s + S[r]) & 0xff;
+			r = r + 1;
+			s = s + S[r];
 			t1 = S[s];
 			S[s] = S[r];
 			S[r] = t1;
@@ -45,10 +45,11 @@ unsigned short pr8()
 		started = true;
 	}
 	//compute the next PR byte using the RC4 encryption algorithm
-	r = (r + 1) & 0xff;
-	s = (s + S[r]) & 0xff;
+	r = r + 1;
+	s = s + S[r];
 	t1 = S[s];
 	S[s] = S[r];
 	S[r] = t1;
-	return S[(S[r] + S[s]) & 0xff];
+	t1 += S[s];
+	return S[t1];
 }
